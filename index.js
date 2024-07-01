2c959b3ede470e48e9efddce2eb93a6f6809f202
Aggy <muneeaggy@gmail.com>
2024-07-01 19:07:31 +0300

    1ST COMMIT

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+console.log(  (Math.floor( Math.random()*10 )+5) );
+
+let num1=2;
+let num2=31;
+let multiply= num1*num2
+console.log(multiply);
+
+let random = Number.parseInt(Math.random()*10+1);
+console.log(random);
+
+let num3=20;
+let num4=8;
+
+let mod = num3 % num4;
+console.log(mod);
+
+let max =Math.max(0,20);
+console.log(max);
